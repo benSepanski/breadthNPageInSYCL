22cfaa25f92c1abc8aef3f2150c4de528b90b309
benSepanski <ben.sepanski@gmail.com>
2020-12-06 17:05:22 -0600

    updated bench

--- bench/run_bfs.sh
+++ bench/run_bfs.sh
@@ -1,28 +1,32 @@
-# Run benchmark with device $1
-for file in `cat graphsToRun.txt` ; do
+# Run benchmark on graphs in file $1 on device $2 with block-sizes in file $3
+for file in `cat $1` ; do
     base=`basename "$file"`;
-    outFile="results/bfs${base%%.gr}${1}.out" ;
+    outFile="results/bfs-${base%%.gr}-dev${2}.out" ;
     if [ -f $outFile ] ; then
         echo "$outFile already exits" ;
     else
         touch $outFile
         echo "Running on $file"
         source_node=`cat ${file%%.gr}.source`;
-        echo "INPUTGRAPH $file" >> $outFile
         for i in {1..3} ; do
-            echo "Device $1" >> $outFile
-            echo "SYCL Data-Driven" >> $outFile
-            ../build/bfs/bfs-data-driven $file -g $1 -s $source_node >>$outFile 2>&1
-        done
-        for i in {1..3} ; do
-            echo "Device $1" >> $outFile
-            echo "SYCL Topology-Driven" >> $outFile
-            ../build/bfs/bfs-topology-driven $file -g $1 -s $source_node >>$outFile 2>&1
-        done
-        for i in {1..3} ; do
-            echo "Device $1" >> $outFile
+            echo "Device $2" >> $outFile
             echo "Lonestar" >> $outFile
-            ../build/extern/Galois/lonestar/analytics/gpu/bfs/bfs-gpu $file -g $1 -s $source_node >>$outFile 2>&1
+            ../build/extern/Galois/lonestar/analytics/gpu/bfs/bfs-gpu $file -g $2 -s $source_node >>$outFile 2>&1
+        done
+        for blocks in `cat $3` ; do
+            echo "INPUTGRAPH $file" >> $outFile
+            for i in {1..3} ; do
+                echo "Device $2" >> $outFile
+                echo "SYCL Data-Driven" >> $outFile
+                ../build/bfs/bfs-data-driven $file -g $2 -b $blocks -s $source_node >>$outFile 2>&1
+            done
+        done
+        for blocks in `cat $3` ; do
+            for i in {1..3} ; do
+                echo "Device $2" >> $outFile
+                echo "SYCL Topology-Driven" >> $outFile
+                ../build/bfs/bfs-topology-driven $file -g $2 -b $blocks -s $source_node >>$outFile 2>&1
+            done
         done
     fi
 done
--- bench/run_pagerank.sh
+++ bench/run_pagerank.sh
@@ -1,27 +1,32 @@
-# Run benchmark with device $1
-for file in `cat graphsToRun.txt` ; do
+# Run benchmark on graphs in file $1 on device $2 with block-sizes in file $3
+MAXIT=5000;
+for file in `cat $1` ; do
     base=`basename "$file"`;
-    outFile="results/pagerank${base%%.gr}${1}.out" ;
+    outFile="results/pagerank-${base%%.gr}-dev${2}.out" ;
     if [ -f $outFile ] ; then
         echo "$outFile already exits" ;
     else
-        touch $outFile ;
+        touch $outFile
         echo "Running on $file"
-        echo "INPUTGRAPH $file" >> $outFile
         for i in {1..3} ; do
-            echo "Device $1" >> $outFile
-            echo "SYCL Data-Driven" >> $outFile
-            ../build/pagerank/pagerank-data-driven $file -g $1 -x 5000 >>$outFile 2>&1
-        done
-        for i in {1..3} ; do
-            echo "Device $1" >> $outFile
-            echo "SYCL Topology-Driven" >> $outFile
-            ../build/pagerank/pagerank-topology-driven $file -g $1 -x 5000 >>$outFile 2>&1
-        done
-        for i in {1..3} ; do
-            echo "Device $1" >> $outFile
+            echo "Device $2" >> $outFile
             echo "Lonestar" >> $outFile
-            ../build/extern/Galois/lonestar/analytics/gpu/pagerank/pagerank-gpu $file -g $1 -x 5000 >>$outFile 2>&1
+            ../build/extern/Galois/lonestar/analytics/gpu/pagerank/pagerank-gpu $file -g $2 -x $MAXIT >>$outFile 2>&1
+        done
+        for blocks in `cat $3` ; do
+            echo "INPUTGRAPH $file" >> $outFile
+            for i in {1..3} ; do
+                echo "Device $2" >> $outFile
+                echo "SYCL Data-Driven" >> $outFile
+                ../build/pagerank/pagerank-data-driven $file -g $2 -b $blocks -x $MAXIT >>$outFile 2>&1
+            done
+        done
+        for blocks in `cat $3` ; do
+            for i in {1..3} ; do
+                echo "Device $2" >> $outFile
+                echo "SYCL Topology-Driven" >> $outFile
+                ../build/pagerank/pagerank-topology-driven $file -g $2 -b $blocks -x $MAXIT >>$outFile 2>&1
+            done
         done
     fi
 done
